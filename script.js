40d2274948e3fd588d9d868399e5aa5f12fd7d54
Rasmus Meinecke <rasmusfabius83@gmai.com>
2019-10-20 11:28:14 +0200

    Final

--- script.js
+++ script.js
@@ -3,22 +3,27 @@ const HOURARM = document.querySelector("#hour");
 const MINUTEARM = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
-function runTheClock() {
-  // get the hours, minutes and secounds from the Date methods .
-  var date = new Date();
-  let hours = date.getHours();
-  let minutes = date.getMinutes();
-  let seconds = date.getSeconds();
+// get the hours, minutes and seconds from the Date methods .
+var date = new Date();
+let hours = date.getHours();
+let minutes = date.getMinutes();
+let seconds = date.getSeconds();
 
-  // Calculate the position for the hours, minute and second arms. With 360 degress
-  let hourPosition = (hours * 360) / 12 + (minutes * (360 / 60)) / 12;
-  let minutePosition = (minutes * 360) / 60 + (seconds * (360 / 60)) / 60;
-  let secondPosition = (seconds * 360) / 60;
+// Calculate the position for the hours, minute and second arms. With 360 degrees
+let hourPosition = (hours * 360) / 12 + (minutes * (360 / 60)) / 12;
+let minutePosition = (minutes * 360) / 60 + (seconds * (360 / 60)) / 60;
+let secondPosition = (seconds * 360) / 60;
+
+// Fix the issuce where the second arm goes back to start every time it hits 12.
+function runTheClock() {
+  hourPosition = hourPosition + 3 / 360;
+  minutePosition = minutePosition + 6 / 60;
+  secondPosition = secondPosition + 6;
 
   HOURARM.style.transform = "rotate(" + hourPosition + "deg)";
   MINUTEARM.style.transform = "rotate(" + minutePosition + "deg)";
   SECONDHAND.style.transform = "rotate(" + secondPosition + "deg)";
 }
 
-// Vi run the runTheClock function every second
+// We run the runTheClock function every second
 var interval = setInterval(runTheClock, 1000);
